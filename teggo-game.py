c9ed6ccba11a1210992013ec097d49bffcfaa3ff
Ascyii <jonashahn1@gmx.net>
2021-12-10 22:24:51 -0800

    Creating project and Initialize pygame

---
+++ teggo-game.py
@@ -0,0 +1,17 @@
+# The Teggo Game by Jonas Hahn 2021
+# This is the main game file
+# I will first program everything in here, and
+# then I will capsule modules later on
+
+import pygame
+
+pygame.mixer.init()
+pygame.init()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
